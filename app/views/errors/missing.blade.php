ae803434709b45240d7bf4b4fa3873b99bc8404b
jonathan robinson <jonbrobinson@gmail.com>
2014-07-01 11:35:33 -0500

    Created folder to add custom error message

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('content')
+<h2>Whoomp, There It's Not</h2>
+<p>
+	We're Having some technical difficulties. PLease be a little patient as we fix this issue
+</p>
+@stop
