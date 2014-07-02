800fcc5b4e94e5c38906ec40e365f06e501a643b
jonathan robinson <jonbrobinson@gmail.com>
2014-07-02 09:38:16 -0500

    Reformated form structure using Divs

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,26 +1,21 @@
 @extends('layouts.master')
 
-
-@section('topscript')
-@stop
 @section('content')
-	<h1>Simple Form</h1>
-	@if ($errors->has('title'))
-	{{{ $errors->first('title', '<span class="help-block">:message</span>' )}}};
-	@endif
-	<form action="{{{ action('PostsController@store') }}}" method="post">
-		<p>
-	        <label for="title">Title</label>
-	        <input id="title" name="title" type="text" value="{{{ Input::old('title') }}}" placeholder="Title">
-	    </p>
-	    <p>
-	        <label for="body">Body</label>
-	        <textarea id="body" name="body"placeholder="Message">{{{ Input::old('body') }}}</textarea>
-	    </p>
-	    <p>
-	        <button type="submit">Submit</button>
-	    </p>
-	</form>
-@stop
-@section('bottomscript')
+
+	<h1>Create Post</h1>
+
+	{{Form::open(array('action' => 'PostsController@store', 'method' => 'POST'))}}
+		<div>
+			{{ Form::label('title', 'Title') }}<br>
+			{{ Form::text('title') }}<br>
+			{{ $errors->first('title', '<<span class="help-block">:mesage</span>')}}<br>
+		</div>
+		<div>
+			{{ Form::label('body', 'Body') }}<br>
+			{{ Form::textarea('body') }}<br>
+			{{ $errors->first('body', '<<span class="help-block">:mesage</span>')}}
+		</div>
+		</div>
+		{{Form::submit('Save Post')}}
+	{{ Form::close() }}
 @stop
