6d66511f3cffae91a21e20dac6065c2fc0dd54a0
jonathan robinson <jonbrobinson@gmail.com>
2014-07-02 14:30:12 -0500

    Added If statements to check error Messages for Users Input

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -8,6 +8,12 @@
     @yield('topscript')
 </head>
 <body id="{{{ $body_id or "" }}}"  class="{{{ $body_class or "" }}}">
+	@if (Session::has('successMessage'))
+		<div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
+	@endif
+	@if (Session::has('errorMessage'))
+		<div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
+	@endif
     @yield('content')
     @yield('bottomscript')
 </body>
