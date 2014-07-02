a5b4819981ef80e91d99207a5f943303a38f44b8
jonathan robinson <jonbrobinson@gmail.com>
2014-07-02 18:32:48 -0500

    Added Seeders for User and Post

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,39 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('PostsTableSeeder');
 	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        $user = new User();
+        $user->email = 'admin@codeup.com';
+        $user->password = Hash::make('adminPass123!');
+        $user->save();
+    }
+
+}
+
+class PostsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('posts')->delete();
+
+        for ($i = 0; $i <= 10; $i++)
+        {
+        $post = new Post();
+        $post->title = "Title $i";
+        $post->body = "This is body text for post $i";
+        $post->save();
+    	}
+    }
 
 }
