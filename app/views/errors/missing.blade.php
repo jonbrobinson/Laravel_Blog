f5e707cbf1795fa1656f40b7d72e8a024311f0c5
jonathan robinson <jonbrobinson@gmail.com>
2014-07-02 14:28:46 -0500

    Changed error header Message

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('content')
-<h2>Whoomp, There It's Not</h2>
+<h2>Something Went Wrong</h2>
 <p>
 	We're Having some technical difficulties. PLease be a little patient as we fix this issue
 </p>
