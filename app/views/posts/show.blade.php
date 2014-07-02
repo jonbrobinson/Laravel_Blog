9c872d01587a54e52a9892d9401bdabb03d1c1e1
jonathan robinson <jonbrobinson@gmail.com>
2014-07-02 14:32:58 -0500

    Added delete form to remove post

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -4,4 +4,8 @@
 @section('content')
 	<h3>{{{$post->title}}}</h3>
 	<p>{{{$post->body}}}</p>
+
+	{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE'))  }}
+		{{ Form::submit('Delete') }}
+	{{ Form::close() }}
 @stop
