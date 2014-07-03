1e331db7bfd4005e2c1ca2e3cb2cb41e5fb720e8
jonathan robinson <jonbrobinson@gmail.com>
2014-07-03 10:46:36 -0500

    Reformatted layout int table.  Added Create date to show

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -4,12 +4,18 @@
 @section('content')
 
 	<h2>My Blog</h2>
-
-	@foreach ($posts as $post)
-		{{ link_to_action('PostsController@show', $post->title, array($post->id)) }}
-		<br>
-	@endforeach
-
+	<table>
+		<tr>
+			<th>Title</th><th>Creation Date</th><th>Action</th>
+		</tr>
+		@foreach ($posts as $post)
+		<tr>
+			<td><a href="{{ action('PostsController@show', $post->id) }}">{{{ $post->title }}}</a></td>
+			<td>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
+			<td><a href="{{ action('PostsController@edit', $post->id) }}" >Edit</a></td>
+		</tr>
+		@endforeach
+	</table>
 	<br>
 	{{ $posts->links() }}
 	<hr>
