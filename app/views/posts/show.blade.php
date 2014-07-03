ad44f9dfbfbea5f6c88539782973eca449b746d1
jonathan robinson <jonbrobinson@gmail.com>
2014-07-03 10:47:37 -0500

    Added create date to show on post

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,6 +3,7 @@
 
 @section('content')
 	<h3>{{{$post->title}}}</h3>
+	<p>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</p>
 	<p>{{{$post->body}}}</p>
 
 	{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE'))  }}
