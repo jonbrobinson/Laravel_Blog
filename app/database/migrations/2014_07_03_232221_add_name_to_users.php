1084e976abe5213afb21825490ca3597c5b4f52e
jonathan robinson <jonbrobinson@gmail.com>
2014-07-04 11:15:45 -0500

    Created Migration to add user names to Database

---
+++ app/database/migrations/2014_07_03_232221_add_name_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNameToUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('first_name', 20);
+			$table->string('last_name', 50);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('first_name');
+			$table->dropColumn('last_name');
+		});
+	}
+
+}
