83798a0ddf0f151eebbd493e12a0362ffdec03de
jonathan robinson <jonbrobinson@gmail.com>
2014-07-04 11:20:36 -0500

    Added Bootstrap Styling

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,18 +3,29 @@
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 
     <title>{{{ $title or "Laravel Blog" }}}</title>
     @yield('topscript')
 </head>
+
+<div class="container">
 <body id="{{{ $body_id or "" }}}"  class="{{{ $body_class or "" }}}">
+
+	<!------ Alert Messages ------>
 	@if (Session::has('successMessage'))
 		<div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
 	@endif
 	@if (Session::has('errorMessage'))
 		<div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
 	@endif
+
     @yield('content')
+ </div>
     @yield('bottomscript')
 </body>
 </html>
