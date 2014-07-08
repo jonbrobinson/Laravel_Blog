17de9e8498f5b0aa087a49032ef15326c9683360
jonathan robinson <jonbrobinson@gmail.com>
2014-07-08 14:19:09 -0500

    Added Method to Render Body

--- app/models/post.php
+++ app/models/post.php
@@ -5,9 +5,37 @@ class Post extends BaseModel {
 	//========The db table this model relates to
     protected $table = 'posts';
 
+    protected $imgDir = 'img-upload';
+
     //=========Validation rules for our model
     static public $rules = [
     	'title' => 'required|max:100',
     	'body' => 'required'
     ];
+
+    public function user()
+	{
+	    return $this->belongsTo('User');
+	}
+
+	public function addUploadImage($image)
+	{
+
+		$systemPath = public_path() . '/' . $this->imgDir . '/';
+
+		$imageName = $this->id . '-' . $image->getClientOriginalNAme();
+
+		$image->move($systemPath, $imageName);
+
+		$this->img_path = '/' . $this->imgDir . '/' . $imageName;
+	}
+
+	public function renderBody ()
+	{
+		$dirty_html = Parsedown::instance()->parse($this->body);
+		$config = HTMLPurifier_Config::createDefault();
+		$purifier = new HTMLPurifier($config);
+		$clean_html = $purifier->purify($dirty_html);
+		return $clean_html;
+	}
 }
