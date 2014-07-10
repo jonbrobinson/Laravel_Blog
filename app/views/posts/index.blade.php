fc75c831c32957605e4ac64428f9758d769c0afc
jonathan robinson <jonbrobinson@gmail.com>
2014-07-10 18:05:53 -0500

    Changed Links Location, Added Class to search field, Reformated Blog Post Display

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -3,11 +3,9 @@
 
 @section('content')
 
-<h2>
-	A Laravel Blog
-</h2>
-<div class="col-md-4">
+<div class="col-md-3">
 	<div class="row">
+		<h2>A Laravel Blog</h2>
 		{{ HTML::image('/img/IMG_3662.png', 'jonathanEnergy', array('class' => 'img-circle', 'height' => '142')) }}
 	</div>
 	<div class="row">
@@ -15,57 +13,40 @@
 		<p>This is a Blog I built using Laravel, a PHP Framework, and designed using Twitter Bootstrap.  THe Post are here to give a little insight into programmer as they search or there next position. I recently jumped into programming and I am excited for the future.  To see a little outside my programming life </p>
 	</div>
 	<div class="row">
-		<h4>Social</h4>
-		<a href="mailto:jonbrobinson@gmail.com"><i class="fa fa-spin fa-envelope fa-3x"></i></a>
-		<a href="http://www.facebook.com/jonbrobinson"><i class="fa fa-facebook fa-3x"></i></a>
-		<a href="http://www.linkedin.com/pub/jonathan-robinson/13/36a/188"><i class="fa fa-linkedin fa-3x"></i></a>
-		<a href="http://www.github.com/jonbrobinson"><i class="fa fa-spin fa-github fa-3x"></i></a>
+		<h4>Let's Connect</h4>
+		<ol class='breadcrumb'>
+			<li><a href="http://www.linkedin.com/pub/jonathan-robinson/13/36a/188"><i class="fa fa-linkedin fa-2x"></i></a></li>
+			<li><a href="mailto:jonbrobinson@gmail.com"><i class="fa fa-envelope fa-2x"></i></a></li>
+			<li><a href="http://www.github.com/jonbrobinson"><i class="fa fa-github fa-2x"></i></a></li>
+			<li><a href="http://www.facebook.com/jonbrobinson"><i class="fa fa-facebook fa-2x"></i></a><br></li>
+			<li><a href="http://www.instagram.com/jonbrobinson"><i class="fa fa-instagram fa-2x"></i></a><br></li>
+		</ol>
+
+		<br>
+
 	</div>
-</div>
-<div class="col-md-8">
-	<div class="row"> <!------ Search Field ------>
-		<hr>
+	<div class="row well"> <!------ Search Field ------>
 		{{Form::open(array('action' => array('PostsController@index'), 'method' => 'GET'))}}
-			{{ Form::text('search', null, array('placeholder' => 'Search Query')) }}
+			{{ Form::label('search', 'Blog Search') }} <br>
+			{{ Form::text('search', null, array('placeholder' => 'Search Blog')) }}
 			{{ Form::submit('Search') }}
-			@if(Auth::check())
-			{{ link_to_action('PostsController@create', 'Create Post', null, array("class" => "btn btn-sm btn-success pull-right", "role" => "button")) }}
-			@endif
 		{{ Form::close() }}
 	</div>
-	<!------ Table to Display Blog Post ------>
+</div>
+<div class="col-md-6 col-md-offset-1">
+	<!------ Blog Post ------>
 	<div class="row">
-		<table class="table table-striped">
-			<tr>
-				<th>
-					Title
-				</th>
-				<th>
-					User
-				</th>
-				<th>
-					Creation Date
-				</th>
-				@if(Auth::check())
-				<th>
-					Action
-				</th>
-				@endif
-			</tr>
-			@foreach ($posts as $post)
-			<tr>
-				<td><a href="{{ action('PostsController@show', $post->id) }}">{{{ $post->title }}}</a></td>
-				<td>{{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</td>
-				<td>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</td>
-				@if(Auth::check())
-				<td>
-					<a href="{{ action('PostsController@edit', $post->id) }}" >Edit</a>
-					<a href="#" class=" deletePost btn btn-default btn-sm" data-postid="{{ $post->id }}">Delete</a>
-				</td>
-				@endif
-			</tr>
-			@endforeach
-		</table> <!-- End Table -->
+		@foreach ($posts as $post)
+			<p><a href="{{ action('PostsController@show', $post->id) }}"><h1>{{{ $post->title }}}</h1></a></p>
+			<p>By: {{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</p>
+			<p><i class="fa fa-clock-o"></i> <strong>Posted on {{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</strong></p>
+			@if($post->img_path)
+				<img src="{{ $post->img_path }}" class="">
+			@endif
+			<p>{{ (str_limit($post->renderBody(), $limit = 300, $end = '...')) }}</p>
+			<a href="{{ action('PostsController@show', $post->id) }}" class=" btn  btn-primary">Read More</a>
+			<hr>
+		@endforeach
 	</div>
 
 	{{ $posts->appends(array('search' => Input::get('search')))->links() }}
