27ee91a26d456ccb1252c611eda358d414625833
jonathan robinson <jonbrobinson@gmail.com>
2014-07-11 14:03:40 -0500

    Added Javasript for session Flash

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,20 +3,33 @@
 
 @section('content')
 <div class="col-md-8 col-md-offset-2">
-	<h2>{{{$post->title}}}{{ link_to_action('PostsController@index', 'Home', null, array("class" => "btn btn-xs btn-primary pull-right", "role" => "button")) }}</h2>
-	<h4>Author: {{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</h4>
+	<h2>{{{$post->title}}}</h2>
+	<h5>by: {{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</h5>
 	<p>{{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</p>
-	<p>{{ $post->renderBody() }}</p>
-
 	@if($post->img_path)
 		<img src="{{ $post->img_path }}" class="img-repsonsive">
 	@endif
-
+	<p>{{ $post->renderBody() }}</p>
+	<div "row">
 	@if(Auth::check())
-	{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'DELETE'))  }}
-		{{ Form::submit('Delete') }}
-	{{ Form::close() }}
-	<a href="{{ action('PostsController@edit', $post->id) }}" >Edit</a>
+		<a href="{{ action('PostsController@edit', $post->id) }}" class="btn btn-primary btn-sm" >Edit</a>
+		<a href="#" class=" deletePost btn btn-danger btn-sm" data-postid="{{ $post->id }}">Delete</a>
 	@endif
+	</div>
+
+	{{ Form::open(array("action" => "PostsController@destroy", 'id' => 'deleteForm', 'method' => 'delete')) }}
+	{{ Form::close() }}
 </div>
+@stop
+
+@section('bottomscript')
+<script type="text/javascript">
+   $(".deletePost").click(function() {
+       var postId = $(this).data('postid');
+       $("#deleteForm").attr('action', '/posts/' + postId);
+       if(confirm("Are you sure you want to delete post")) {
+           $('#deleteForm').submit();
+       }
+   });
+</script>
 @stop
