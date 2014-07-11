9ddd63f1c5fe9b34a2dcf50aba832573c7f861e9
jonathan robinson <jonbrobinson@gmail.com>
2014-07-11 14:12:19 -0500

    Add Hidden User

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -24,8 +24,8 @@ class UserTableSeeder extends Seeder {
         DB::table('users')->delete();
 
         $user = new User();
-        $user->email = 'admin@codeup.com';
-        $user->password = Hash::make('adminPass123!');
+        $user->email = $_ENV['ADMIN_USER'];
+        $user->password = Hash::make($_ENV['ADMIN_USER']);
         $user->first_name = 'Jonathan';
         $user->last_name = 'Robinson';
         $user->save();
