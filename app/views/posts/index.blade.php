be8b4e7bc8e76d6f102cae40e32605a11b8e6ba6
jonathan robinson <jonbrobinson@gmail.com>
2014-07-11 09:31:00 -0500

    Changed URL to find by Slug

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -37,14 +37,14 @@
 	<!------ Blog Post ------>
 	<div class="row">
 		@foreach ($posts as $post)
-			<p><a href="{{ action('PostsController@show', $post->id) }}"><h1>{{{ $post->title }}}</h1></a></p>
+			<p><a href="{{ action('PostsController@show', $post->slug) }}"><h1>{{{ $post->title }}}</h1></a></p>
 			<p>By: {{{ $post->user->first_name }}} {{{ $post->user->last_name }}}</p>
 			<p><i class="fa fa-clock-o"></i> <strong>Posted on {{{ $post->created_at->format('l, F jS Y @ h:i:s A') }}}</strong></p>
 			@if($post->img_path)
 				<img src="{{ $post->img_path }}" class="">
 			@endif
 			<p>{{ (str_limit($post->renderBody(), $limit = 300, $end = '...')) }}</p>
-			<a href="{{ action('PostsController@show', $post->id) }}" class=" btn  btn-primary">Read More</a>
+			<a href="{{ action('PostsController@show', $post->slug) }}" class=" btn  btn-primary">Read More</a>
 			<hr>
 		@endforeach
 	</div>
