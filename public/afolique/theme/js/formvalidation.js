8db8c667cebad0a107f72128a24047e1e48aa771
jonathan robinson <jonbrobinson@gmail.com>
2014-09-01 16:54:13 -0500

    adjusted AJAX url

--- public/afolique/theme/js/formvalidation.js
+++ public/afolique/theme/js/formvalidation.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
 		//start the ajax
 		$.ajax({
 			//this is the php file that processes the data and sends email
-			url: "/afolique/theme/contact.php",
+			url: "afolique/theme/contact.php",
 
 			//GET method is used
 			type: "GET",
